3d9d523e6b7ef46e67eabadd6d98a96959dfbbde
quanghungle711 <quanghung07112003@gmail.com>
2025-02-14 20:28:51 +0700

    DeleteEmail
    DeleteEmail

---
+++ MailCrafter.MVC/Controllers/DeleteEmailAccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MailCrafter.MVC.Controllers
+{
+    public class DeleteEmailAccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
