270365ab661ec0ca8eec9446ae0aeff6e147bfb0
quanghungle711 <quanghung07112003@gmail.com>
2025-02-15 11:17:22 +0700

    Update DeleteEmailAccount
    Update delete

--- MailCrafter.MVC/Controllers/DeleteEmailAccountController.cs
+++ MailCrafter.MVC/Controllers/DeleteEmailAccountController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MailCrafter.Domain;
+using MailCrafter.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.Extensions.Logging;
 
 namespace MailCrafter.MVC.Controllers
 {
     public class DeleteEmailAccountController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<DeleteEmailAccountController> _logger;
+        private readonly IAppUserService _userService;
+
+        public DeleteEmailAccountController(ILogger<DeleteEmailAccountController> logger, IAppUserService userService)
         {
-            return View();
+            _logger = logger;
+            _userService = userService;
+        }
+
+        [HttpPost]
+        [Route("management/email-accounts/delete")]
+        public async Task<IActionResult> DeleteEmailAccount([FromBody] string email)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                var result = await _userService.RemoveEmailAccount(userId, email);
+
+                if (result.IsSuccessful)
+                {
+                    return Ok(new { message = "Email account deleted successfully." });
+                }
+
+                ModelState.AddModelError(string.Empty, "Failed to delete email account.");
+            }
+
+            return BadRequest(new { errors = ModelState });
         }
     }
 }
