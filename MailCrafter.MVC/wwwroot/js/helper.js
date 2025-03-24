5453661092260fdb60b301d66cf65fc72047ae2e
Vu Tran <vutt1808@gmail.com>
2025-03-24 13:54:35 +0700

    update dashboard

--- MailCrafter.MVC/wwwroot/js/helper.js
+++ MailCrafter.MVC/wwwroot/js/helper.js
@@ -35,3 +35,107 @@
         removeToast();
     });
 }
+
+document.querySelectorAll('.form-select').forEach(function (select) {
+    var selectOptions = select.querySelectorAll('option');
+
+    select.classList.add('hide-select');
+    var wrapper = document.createElement('div');
+    wrapper.className = 'select';
+    select.parentNode.insertBefore(wrapper, select);
+    wrapper.appendChild(select);
+
+    var customSelect = document.createElement('div');
+    customSelect.className = 'custom-select';
+    customSelect.textContent = selectOptions[0].textContent;
+    wrapper.appendChild(customSelect);
+
+    var optionList = document.createElement('ul');
+    optionList.className = 'select-options';
+    wrapper.appendChild(optionList);
+
+    selectOptions.forEach(function (option) {
+        var listItem = document.createElement('li');
+        listItem.textContent = option.textContent;
+        listItem.setAttribute('rel', option.value);
+        optionList.appendChild(listItem);
+
+        listItem.addEventListener('click', function (e) {
+            e.stopPropagation();
+            customSelect.textContent = this.textContent;
+            select.value = this.getAttribute('rel');
+            optionList.style.display = 'none';
+            customSelect.classList.remove('active');
+        });
+    });
+
+    customSelect.addEventListener('click', function (e) {
+        e.stopPropagation();
+        document.querySelectorAll('.custom-select.active').forEach(function (activeSelect) {
+            if (activeSelect !== customSelect) {
+                activeSelect.classList.remove('active');
+                activeSelect.nextElementSibling.style.display = 'none';
+            }
+        });
+        customSelect.classList.toggle('active');
+        optionList.style.display = optionList.style.display === 'block' ? 'none' : 'block';
+    });
+
+    document.addEventListener('click', function () {
+        customSelect.classList.remove('active');
+        optionList.style.display = 'none';
+    });
+});
+
+const renderChart = (containerId, chartData) => {
+    Highcharts.chart(containerId, {
+        chart: {
+            height: 300,
+            plotBackgroundColor: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+
+        },
+        title: {
+            text: '8',
+            align: 'center',
+            verticalAlign: 'middle',
+            style: {
+                fontSize: '40px',
+                fontWeight: 'bold',
+                color: '#333'
+            }
+        },
+        tooltip: {
+            pointFormat: '<b>{point.name}: </b>{point.percentage:.1f}%'
+        },
+        plotOptions: {
+            pie: {
+                dataLabels: {
+                    enabled: false
+                },
+                startAngle: 0,
+                endAngle: 360,
+                center: ['50%', '50%'],
+                size: '100%'
+            }
+        },
+        series: [{
+            type: 'pie',
+            innerSize: '80%',
+            data: chartData
+
+        }],
+        credits: {
+            enabled: false // Ẩn dòng text "Highcharts.com"
+        }
+    });
+};
+const data1 = [
+    { name: 'Processing', y: 8, color: '#FFA500' }, // Orange for processing
+    { name: 'Completed', y: 2, color: '#4CAF50' }  // Green for completed
+];
+
+renderChart('task-container-chart', data1);
+renderChart('task-container-chart2', data1);
+renderChart('task-container-chart3', data1);
